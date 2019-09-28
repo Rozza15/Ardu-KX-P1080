327d0a06019d5f16d7e32cbe16c2f4277f732f0b
Rory Buchanan <rbuchanan.1997@gmail.com>
2019-09-28 10:33:56 +1000

    Update DotMatrxDriver.ino
    Connected it to the printer and trying to output controls. Somewhat working, somewhat gibberish

--- DotMatrxDriver/DotMatrxDriver.ino
+++ DotMatrxDriver/DotMatrxDriver.ino
@@ -1,40 +1,63 @@
 /*  KX-P1081 Control  */
 //  Ard->Prn
-int STROBE = 9;
-int AFXT = 4;
-int PRIME = 3;
+int STROBE = 9; //  Active Low
+int AFXT = 4; //  Active Low
+int PRIME = 3;  //  Active Low
 //  Ard<-Prn
-int ACK = 8;
-int BUSY = 7;
-int PE = 6;
-int SLCT = 5;
-int ERR = 2;
+int ACK = 8;  //  Active Low
+int BUSY = 7; //  Active High
+int PE = 6; //  Active High
+int SLCT = 5; //  Active High
+int ERR = 2;  //  Active Low
 
 
 /*  595 Register Control  */
 int dataPin = 10;	//	595_DS
-int outPin = 16;	//	595_OE
+int outPin = 16;	//	595_OE - active low. Write high to blank, write low to show
 int latchPin = 14;	//	595_ST_CP
 int clockPin = 15;	//	595_SH_CP
 int clearPin = 18;	//	595_MR
 
-const byte payloadArray[13] = {0x48,0x65,0x6C,0x6C,0x6F,0x2C,0x20,0x77,0x6F,0x72,0X6C,0x64,0x21};
+const byte payloadArray[19] = {0x1B,0x40,0x1B,0x23,0x48,0x65,0x6C,0x6C,0x6F,0x2C,0x20,0x77,0x6F,0x72,0X6C,0x64,0x21,0x1B,0x3C};
 
 
 
 byte data;
 byte dataArray[10];
+int i = 0;
 
 void setup() {
   Serial.begin(9600);
+
+/*  Initialise control ouputs */
+//  595 Shift Lines
   pinMode(dataPin, OUTPUT);
   pinMode(outPin, OUTPUT);
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(clearPin, OUTPUT);
 
+//  Printer input controls
+  pinMode(STROBE, OUTPUT);
+  pinMode(AFXT, OUTPUT);
+  pinMode(PRIME, OUTPUT);
+
+/*  Initialise control inputs */
+//  Printer output controls
+  pinMode(ACK, INPUT);
+  pinMode(BUSY, INPUT);
+  pinMode(PE, INPUT);
+  pinMode(SLCT, INPUT);
+  pinMode(ERR, INPUT);
+
+
   digitalWrite(outPin, 0);
   digitalWrite(clearPin, 1);
+
+  
+  digitalWrite(STROBE, 1);
+  digitalWrite(AFXT, 1);
+  digitalWrite(PRIME, 1);
 
   dataArray[0] = 0xFF; //0b11111111
   dataArray[1] = 0xFE; //0b11111110
@@ -49,14 +72,62 @@ void setup() {
 }
 
 void loop() {
-  byte j = 0x00;
-  for(int i=0;i<256;i++) {
-    data = j+i;
-    digitalWrite(latchPin, 0);
-    shiftOut(dataPin, clockPin, data);
-    digitalWrite(latchPin, 1);
-    delay(300);
-  }
+    int valACK = digitalRead(ACK);
+    int valBUSY = digitalRead(BUSY);
+    int valPE = digitalRead(PE);
+    int valSLCT = digitalRead(SLCT);
+    int valERR = digitalRead(ERR);
+    Serial.print("ACK:\t");
+    Serial.println(valACK);
+    Serial.print("BUSY:\t");
+    Serial.println(valBUSY);
+    Serial.print("PE:\t");
+    Serial.println(valPE);
+    Serial.print("SLCT:\t");
+    Serial.println(valSLCT);
+    Serial.print("ERR:\t");
+    Serial.println(valERR);
+    Serial.println();
+//    while(valBUSY == 0) {
+//        digitalWrite(latchPin, 0);
+//        shiftOut(dataPin, clockPin, data);
+//        digitalWrite(latchPin, 1);
+//        delayMicroseconds(50);
+//        digitalWrite(outPin, 0);
+//        delayMicroseconds(50);
+//        digitalWrite(STROBE, 0);
+//        delayMicroseconds(50);
+//        digitalWrite(STROBE, 1);
+//        valBUSY = digitalRead(BUSY);
+  
+//  byte j = 0x00;
+//  digitalWrite(latchPin, 0);
+//  shiftOut(dataPin, clockPin, j);
+//  digitalWrite(latchPin, 1);
+//  for(int i=0;i<10;i++) {
+//    digitalWrite(outPin, 0);
+//    delay(300);
+//    digitalWrite(outPin, 1);
+//    delay(300);
+//  }
+//  for(int i=0;i<13;i++) {
+//    data = payloadArray[i];
+//    digitalWrite(outPin, 1);
+//    digitalWrite(latchPin, 0);
+//    shiftOut(dataPin, clockPin, data);
+//    digitalWrite(latchPin, 1);
+//    delay(5);
+//    digitalWrite(outPin, 0);
+//    delay(500);
+//    digitalWrite(STROBE, 0);
+//    delay(500);
+//    digitalWrite(STROBE, 1);
+//    delay(5);
+//  }
+//  while (true){
+//    digitalWrite(STROBE, 1);
+//    digitalWrite(outPin, 1);
+//    }
   
 //  testPayload(payloadArray);
 //  while(true) {}
