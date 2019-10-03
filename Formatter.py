6ffdde246d65ed3fcc6018a33ddf45f1892e3e45
Rozza15 <rbuchanan.1997@gmail.com>
2019-10-03 19:06:00 +1000

    Formatter.py
    Sets bold, underline, and italics values

--- Formatter.py
+++ Formatter.py
@@ -14,7 +14,7 @@ plainArray = textwrap.wrap(plainText)
 
 for i in plainArray:
     wordArray = re.split(r'({b}|{i}|{u}|\s+)', i)
-    printerArray += wordArray
+    printerArray += [wordArray]
 
 bold = False
 underline = False
@@ -23,6 +23,23 @@ italics = False
 
 n = 0
 for j in printerArray:
-    print("{}:".format(n), sep=":", end=" ")
-    print(j)
-    n += 1
+    for k in j:
+        if k == "{b}":
+            if bold:
+                bold = False
+            else:
+                bold = True
+        elif k == "{u}":
+            if underline:
+                underline = False
+            else:
+                underline = False
+        elif k == "{i}":
+            if italics:
+                italics = False
+            else:
+                italics = True
+        print("{}:".format(n), sep=":", end=" ")
+        print("b: {}, u: {}, i: {}".format(bold, underline, italics))
+        print(k)
+        n += 1
